45e2876e3204c1ee3a615a763f2a21d16c83fcd1
dlutjwh <40100470+dlutjwh@users.noreply.github.com>
2019-04-27 16:11:30 +0800

    Create 插入排序.cpp

---
+++ 排序/插入排序.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+	void insertionSortArray(int a[], int n) {    //数组形式
+		for (int i = 1;i < n;i++) {
+			for (int j = 0;j < i;j++) {
+				if (a[j] >= a[i] ) {
+					int cnt = a[i];
+					if (i != j) {
+						for (int k = i;k > j;k--)
+							a[k] = a[k - 1];
+					}
+					a[j] = cnt;
+					break;
+				}
+			}
+		}
+	}
+	ListNode* insertionSortList(ListNode* head) {      //链表形式
+		if (head == nullptr)
+			return nullptr;
+		ListNode *first = new ListNode(INT_MIN);
+		
+		first->next = head;
+		while (head->next != nullptr) {
+			
+			while (head->next&&head->next->val>=head->val) 
+				head = head->next;
+			if (head&&head->next!=nullptr) {
+				ListNode *p = first,*q =head->next->next,*m=nullptr;
+				while (p->next&&p->next->val < head->next->val) {
+					p = p->next;
+				}
+				m = p->next;
+				p->next = head->next;
+				p->next->next = m;
+				head->next = q;
+			}
+		}
+		return first->next;
+		
+	}
+};
